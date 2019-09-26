35f7c58634b0f1e8722853bce767fabe9dc64daa
unknown <esquivellopezronlad@hotmail.com>
2019-09-26 07:00:11 -0600

    Creacion de metodo para las combinaciones

--- Caso7/src/caso7/AES.java
+++ Caso7/src/caso7/AES.java
@@ -1,19 +1,20 @@
+package caso7;
+
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
- 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
- 
-public class Caso7 {
- 
+
+public class AES {
+
     private static SecretKeySpec secretKey;
     private static byte[] key;
- 
-    public static void setKey(String myKey)
-    {
+
+    public static void setKey(String myKey) {
         MessageDigest sha = null;
         try {
             key = myKey.getBytes("UTF-8");
@@ -21,45 +22,54 @@ public class Caso7 {
             key = sha.digest(key);
             key = Arrays.copyOf(key, 16);
             secretKey = new SecretKeySpec(key, "AES");
-        }
-        catch (NoSuchAlgorithmException e) {
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
-        }
-        catch (UnsupportedEncodingException e) {
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
- 
-    public static String encrypt(String strToEncrypt, String secret)
-    {
-        try
-        {
+
+    /*public static String encrypt(String strToEncrypt, String secret) {
+        try {
             setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Error while encrypting: " + e.toString());
         }
         return null;
-    }
- 
-    public static String decrypt(String strToDecrypt, String secret)
-    {
-        try
-        {
+    }*/
+    public static String decrypt(String strToDecrypt, String secret) {
+        try {
             setKey(secret);
             Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Error while decrypting: " + e.toString());
         }
         return null;
     }
-}
 
+    //public int numeros[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public char letras[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    public ArrayList<Combinacion> combinaciones;
+
+    public void CrearCombinaciones() {
+        combinaciones = new ArrayList<>();
+        for (int a = 0; a > 10; a++) {
+            for (int b = 0; b > 26; b++) {
+                Combinacion nueva= new Combinacion(a,letras[b]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final String secretKey = "29dh120_dk1_3";
+        String encryptedString = "xZwM7BWIpSjYyGFr9rhpEa+cYVtACW7yQKmyN6OYSCv0ZEg9jWbc6lKzzCxRSSIvOvlimQZBMZOYnOwiA9yy3YU8zk4abFSItoW6Wj0ufQ0=";
+        String decryptedString = decrypt(encryptedString, secretKey);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
+    }
+}
---
+++ Caso7/src/caso7/Combinacion.java
@@ -0,0 +1,17 @@
+
+package caso7;
+
+/**
+ *
+ * @author PC_ESQUIVEL_LOPEZ
+ */
+public class Combinacion {
+    int numero;
+    char letra;
+
+    public Combinacion(int numero, char letra) {
+        this.numero = numero;
+        this.letra = letra;
+    }
+    
+}
